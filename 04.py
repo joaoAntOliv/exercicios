41f686365e632d8748e2cfe3302bb8a03de69f12
joao <132175155+joaoAntOliv@users.noreply.github.com>
2023-05-30 22:17:08 -0300

    Create 04.py

---
+++ 04.py
@@ -0,0 +1,32 @@
+# exercicios
+'''Escreva um programa que receba uma lista de números digitados pelo usuário e 
+exiba o maior número e o menor número da lista.'''
+# to muito ruim, n me julgue
+
+lista = [
+
+]
+
+
+while True:
+
+    adicionar = int(input('Adicionar a lista:(digite 0 para parar) '))
+
+    if adicionar == 0:
+        break
+
+    try:
+        num = float(adicionar)
+        lista.append(num)
+    except ValueError:
+        print('Digite uma resposta válida')
+
+    if len(lista) > 0:
+        menor = min(lista)
+        maior = max(lista)
+    else:
+        print('Nenhum número digitado')
+
+    print(maior)
+    print(menor)
+    
